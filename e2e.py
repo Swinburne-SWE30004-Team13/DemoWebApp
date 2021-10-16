d5e1ed6aeb40895c0a4b983c0f003044685c4f53
Chuong Ho, Dac Thanh (Swinburne University of Technology) <101921623@student.swin.edu.au>
2021-10-17 02:34:57 +1100

    add e2e

--- e2e.py
+++ e2e.py
@@ -1,34 +1,46 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# from time import sleep 
+# from decouple import config
+
+
+# # print out the port 
+# PORT = config('PORT')
+
+# from selenium.webdriver.chrome.options import Options
+
+# options = Options()
+
+
+# options.add_argument("--headless")
+# options.add_argument("--disable-gpu")
+# options.add_argument("--no-sandbox")
+# options.add_argument("enable-automation")
+# options.add_argument("--disable-infobars")
+# options.add_argument("--disable-dev-shm-usage")
+
+# driver = webdriver.Chrome(options=options)
+
+# def test_heading():
+#     heading = driver.find_element_by_class_name("h3")
+#     print(heading.text)
+#     if heading.text != "Team 13":
+#         raise ValueError("Heading not correct!")
+
+# print("Open the test server at ", PORT)
+# driver.get("http://54.66.27.76:7998/")
+# test_heading()
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from time import sleep 
-from decouple import config
-
-
-# print out the port 
-PORT = config('PORT')
-
 from selenium.webdriver.chrome.options import Options
-
 options = Options()
-
-
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("enable-automation")
 options.add_argument("--disable-infobars")
 options.add_argument("--disable-dev-shm-usage")
-
 driver = webdriver.Chrome(options=options)
-
-def test_heading():
-    heading = driver.find_element_by_class_name("h3")
-    print(heading.text)
-    if heading.text != "Team 13":
-        raise ValueError("Heading not correct!")
-    
-    
-
-print("Open the test server at ", PORT)
-driver.get("http://54.66.27.76:7998/")
-test_heading()
+driver.get("https://www.google.com/")
+element_text = driver.page_source
+print(element_text)
