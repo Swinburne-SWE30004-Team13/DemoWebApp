86c33747e1b219f816c317bd6fbce37a0621ce14
Chuong Ho, Dac Thanh (Swinburne University of Technology) <101921623@student.swin.edu.au>
2021-10-17 02:05:37 +1100

    fix e2

--- e2e.py
+++ e2e.py
@@ -63,7 +63,8 @@
 
 #application.py
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Optionsoptions = Options()
+from selenium.webdriver.chrome.options import Options
+options = Options()
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
