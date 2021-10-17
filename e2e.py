c205e698c65b05eb4d6b2af1d05624998721f28a
Chuong Ho, Dac Thanh (Swinburne University of Technology) <101921623@student.swin.edu.au>
2021-10-17 20:40:21 +1100

    first option

--- e2e.py
+++ e2e.py
@@ -34,9 +34,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 options = Options()
+options.add_argument("--no-sandbox")
 options.add_argument("--headless")
 options.add_argument("--disable-gpu")
-options.add_argument("--no-sandbox")
 options.add_argument("enable-automation")
 options.add_argument("--disable-infobars")
 options.add_argument("--disable-dev-shm-usage")
