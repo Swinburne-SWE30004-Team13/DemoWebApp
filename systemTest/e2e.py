7b7227547605beb81e0870fd3c032135ab61fc6d
Chuong Ho, Dac Thanh (Swinburne University of Technology) <101921623@student.swin.edu.au>
2021-10-18 01:42:41 +1100

    change folder

---
+++ systemTest/e2e.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=chrome_options)
+
+def test_heading():
+    heading = driver.find_element(By.TAG_NAME,"h3")
+    print(heading.text)
+    if heading.text != "Team 13":
+        raise ValueError("Heading not correct!")
+
+driver.get("http://13.55.209.93:7998/")
+
+print(driver.page_source)
+test_heading()
