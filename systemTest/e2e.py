05364a31f04a4e50d21cc907f9d154a6b74d17eb
Chuong Ho, Dac Thanh (Swinburne University of Technology) <101921623@student.swin.edu.au>
2021-10-18 01:59:53 +1100

    environmentize

--- systemTest/e2e.py
+++ systemTest/e2e.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from decouple import config
+
+
+# print out the port 
+TEST_SERVER_IP = config('TEST_SERVER_IP')
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
@@ -11,14 +16,14 @@ driver = webdriver.Chrome(options=chrome_options)
 def test_heading():
     heading = driver.find_element(By.TAG_NAME,"h3")
     print("Heading value: ", heading.text)
-    if heading.text == "Team 3":
+    if heading.text == "Team 13":
         print("Pass heading testing")
     else:
         print("Failed heading testing")
         raise ValueError("Heading not correct!")
 
-
-driver.get("http://13.55.209.93:7998/")
+print(f"Accessing {TEST_SERVER_IP}")
+driver.get(TEST_SERVER_IP)
 
 print(driver.page_source)
 test_heading()
