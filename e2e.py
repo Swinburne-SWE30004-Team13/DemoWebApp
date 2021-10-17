ed722b7a7b6f8ad0e6f4eb0572bb902cfbde7bcd
Chuong Ho, Dac Thanh (Swinburne University of Technology) <101921623@student.swin.edu.au>
2021-10-18 01:34:59 +1100

    add e2e

--- e2e.py
+++ e2e.py
@@ -33,6 +33,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
@@ -41,7 +42,7 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome(options=chrome_options)
 
 def test_heading():
-    heading = driver.find_element_by_tag_name("h3")
+    heading = driver.find_element(By.TAG_NAME,"h3")
     print(heading.text)
     if heading.text != "Team 13":
         raise ValueError("Heading not correct!")
@@ -50,3 +51,7 @@ driver.get("http://13.55.209.93:7998/")
 
 print(driver.page_source)
 test_heading()
+
+
+while True:
+    print("Hey")
