c93677daad1fa47c76e53a05f1622cb32ee2e698
JackRepo170 <cr85170@gmail.com>
2021-10-18 22:02:24 +1100

    Demo Presentation

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ app = Flask(__name__, template_folder="view")
 @app.route("/")
 def hello():
     html =  "<h3 id='heading'>Team 13</h3>" \
-            "<b id='student1'>Student Name:</b> Joc k Hyland - 101603196<br/>" \
+            "<b id='student1'>Student Name:</b> Jack Hyland - 101603196<br/>" \
             "<b id='student2'>Student Name:</b> Chuong Ho - 101921623<br/>" \
+            "<p>Project Demo </p>" \
             "<p>Tutorial time: 12:30PM - 14:30PM </p>"
     
     
